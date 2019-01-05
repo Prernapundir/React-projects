348834bab3b91c74eb9acccb62f865d9ccc9cd2a
Prernapundir <prernapundir1098@gmail.com>
2019-01-05 16:19:41 +0530

    Image search app  Completed

--- react/pics/src/components/App.js
+++ react/pics/src/components/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../Api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
 
 class App extends React.Component {
   state = { images: [] };
-  async onSearchSubmit(term){
-    const response = await axios.get('https://api.unsplash.com/search/photos',{
+   onSearchSubmit = async(term)=>{
+    const response = await unsplash.get('/search/photos',{
       params: { query : term },
-      headers: {
-        Authorization : 'Client-ID 0f3940ebc578306d20e271e0dfd7f9031628743ab2d94d9927c7d98c26f502b0'
-      }
+
     });
-    this.setState({images: response.data.results});
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{marginTop:"20px"}}>
       <SearchBar  onSubmit={this.onSearchSubmit} />
-      Found: {this.state.images.length} images
+      <ImageList  images={this.state.images}/>
       </div>
     );
   }
